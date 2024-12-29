1fb40f7b79fdeacc0813d152e0a1c18b909b750a
Amir <amirhossein.d.z@gmail.com>
2024-12-29 02:29:31 +0100

    Refactor Terraform configuration for improved readability and consistency

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,15 +1,15 @@
-provider "aws" {
-  region = "eu-central-1"
-}
-
 terraform {
   backend "s3" {
     bucket = "code-runner-terraform-state"
-    key = "terraform/state"
+    key = "terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraform-lock"
     encrypt = true
   }
+}
+
+provider "aws" {
+  region = "eu-central-1"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
@@ -27,18 +27,17 @@ resource "aws_s3_bucket_acl" "terraform_state_acl" {
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-    name = "terraform-lock"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+  name = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
 
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 
-    tags = {
-        Environment = "Production"
-        Application = "CodeRunner"
-    }
+  tags = {
+    Environment = "Production"
+    Application = "CodeRunner"
+  }
 }
-
