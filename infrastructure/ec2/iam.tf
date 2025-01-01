7dc7d1cfd829fe940abaede993705760c0e99aa0
Amir <amirhossein.d.z@gmail.com>
2025-01-01 01:28:48 +0100

    Add IAM role and instance profile for SSM access in EC2 configuration

---
+++ infrastructure/ec2/iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "ssm_role" {
+  name = "code-runner-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_role_policy" {
+  role = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ssm_instance_profile" {
+  name = "code-runner-ssm-profile"
+  role = aws_iam_role.ssm_role.name
+}
--- infrastructure/ec2/instance.tf
+++ infrastructure/ec2/instance.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "code_runner" {
   key_name        = aws_key_pair.code_runner_key.key_name
   vpc_security_group_ids = [ aws_security_group.code_runner_sg.id ]
   subnet_id = var.private_subnet_id
+  iam_instance_profile = aws_iam_instance_profile.ssm_instance_profile.name
 
   # Install Go and run the app
   user_data = <<-EOF
